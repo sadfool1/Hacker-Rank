c3fd8d429edbf686d23677e510932cfe3571bfc0
James <takw14@gmail.com>
2020-05-15 20:49:13 +0800

    Better Running Time
    Array size 10**3 takes 3 seconds.

--- Hard/Runningtime2.py
+++ Hard/Runningtime2.py
@@ -12,11 +12,12 @@ import math
 import time
 
 
-A = np.array([i* random.randint(2,pow(10,5)) for i in range(1, pow(10,2))])
-B = np.array([i* random.randint(2,pow(10,5)) for i in range(1, pow(10,2))])
+A = np.array([i* random.randint(2,pow(10,5)) for i in range(1, pow(10,3))])
+B = np.array([i* random.randint(2,pow(10,5)) for i in range(1, pow(10,3))])
+
+timeinit = time.process_time()
 
 counter = 0
-timeinit = time.process_time()
 
 def coprime_checker (Ai, Bj):
     """
@@ -31,66 +32,66 @@ def coprime_checker (Ai, Bj):
     a = Ai
     b = Bj
     
-    A_factor = [] #initialise 
-    B_factor = []  #initialise 
+    A_factor = {} #initialise 
+    B_factor = {} #initialise 
     
-    for i in range(1, math.ceil(math.sqrt(Ai)) + 1):
+    for i in range(1, math.ceil(math.sqrt(Ai))):
         if Ai % i == 0:
-            A_factor.append(i)
-            A_factor.append(Ai/i)
+            A_factor[i] = i
+            A_factor[i+1] = Ai/i
+            
             #APPEND THE FACTORS
             
-    for i in range(1, math.ceil(math.sqrt(Bj))+ 1):
+    for i in range(1, math.ceil(math.sqrt(Bj))):
         if Bj % i ==0:
-            B_factor.append(i)
-            B_factor.append(Bj/i)
-            
+            B_factor[i] = i
+            B_factor[i+1] = Bj/i
     
-    for i in range(len(A_factor)):
-        for j in range(len(B_factor)):
+    for i in A_factor:
+        for j in B_factor:
             if A_factor[i] == B_factor[j]:
                 factor_counter = factor_counter + 1
-                
-    if factor_counter > 1:
-        return False
-    else:
+                continue
+            else:
+                pass
+        if factor_counter > 1:
+            break
+    
+    return False if factor_counter > 1 else True
 
-        return True
 
     
-#A = np.array([3, 7, 32])
+#A = np.array([3, 7, 30])
 #B = np.array([6, 14, 15])
 
 def main(A, B):
-    
-    global timeinit
-
+    global counter
     
     tempi = []
     tempj = []
-    
+ 
+
     for i in range(0,len(A)):
+        if len(tempi) > 1:
+            break
+        
         for j in range(0,len(B)):
-            #print ("i = %d j = %d." % (i,j), end = '')
-            
             if coprime_checker(int(A[i]), int(B[j])) == True:
                 pass
-            
-            else:
-                #print ("Deleting %d & %d" %(A[i], B[j]), end = '')
-                
+            else:    
                 tempi.append(i)
                 tempj.append(j)
-                
                 break
-                
+        
+            
+
     deleter(A, B, tempi[0],tempj[0])
-    
+
 
 def deleter(A, B, a,b):
     global counter
     global timeinit
-    
+
     try:
         counter = counter + 1
         
@@ -104,5 +105,6 @@ def deleter(A, B, a,b):
         timer =  timeend - timeinit
         print ("The total Counter for this method = %d." % counter)
         print ("Time taken to execute: %f seconds " % timer)
-        
+
+
 main(A, B) #initiliase the code
