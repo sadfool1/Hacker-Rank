744ff5b811261d359ade76d7077c1410f4d8dc10
James <takw14@gmail.com>
2020-05-15 21:40:45 +0800

    Question Added

--- Hard/Final.py
+++ Hard/Final.py
@@ -4,6 +4,50 @@
 Created on Thu May 14 15:17:12 2020
 
 @author: jameselijah
+
+Sophia is playing a game on the computer. There are two random arrays A & B, each having the same number of elements. The game begins with Sophia removing a pair (Ai, Bj) from the array if they are not co-prime. She keeps a count on the number of times this operation is done.
+
+Sophia wants to find out the maximal number of times(S) she can do this on the arrays. Could you help Sophia find the value?
+
+Input Format
+
+The first line contains an integer n. 2 lines follow, each line containing n numbers separated by a single space. The format is shown below.
+
+Input Format
+
+The first line contains an integer n. 2 lines follow, each line containing n numbers separated by a single space. The format is shown below.
+
+n
+A[0] A[1] ... A[n - 1]
+B[0] B[1] ... B[n - 1]
+Constraints
+
+0 < n <= 105
+2 <= A[i], B[i] <= 109
+Each element in both arrays are generated randomly between 2 and 109
+
+Output Format
+
+Output S which is the maximum number of times the above operation can be made.
+
+Sample Input
+4
+2 5 6 7
+4 9 10 12
+Sample Output
+
+3
+
+Explanation:
+
+You can remove:
+
+(2, 4)
+(5, 10)
+(6, 9)
+
+hence 3//
+
 """
 
 import numpy as np
