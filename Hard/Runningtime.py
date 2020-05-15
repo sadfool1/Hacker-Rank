a79ef3a15635a2b575ddfde61435cb4dbe00a3fa
James <takw14@gmail.com>
2020-05-15 19:25:36 +0800

    Optimised Coprime Checker

--- Hard/Runningtime.py
+++ Hard/Runningtime.py
@@ -19,7 +19,13 @@ import time
 counter = 0
 
 def coprime_checker (Ai, Bj):
-    
+    """
+    =========================
+    Checks Factors of
+    Integers then returns
+    Boolean if Coprime or not
+    =========================
+    """
     factor_counter = 0
     
     a = Ai
@@ -28,34 +34,35 @@ def coprime_checker (Ai, Bj):
     A_factor = [] #initialise 
     B_factor = []  #initialise 
     
-    for i in range(1, Ai + 1):
+    for i in range(1, math.ceil(math.sqrt(Ai)) + 1):
         if Ai % i == 0:
             A_factor.append(i)
+            A_factor.append(Ai/i)
+            #APPEND THE FACTORS
             
-    for i in range(1, Ai + 1):
+    for i in range(1, math.ceil(math.sqrt(Bj))+ 1):
         if Bj % i ==0:
             B_factor.append(i)
-        
+            B_factor.append(Bj/i)
+            
+    
     for i in range(len(A_factor)):
         for j in range(len(B_factor)):
             if A_factor[i] == B_factor[j]:
                 factor_counter = factor_counter + 1
+                
     if factor_counter > 1:
         return False
     else:
 
         return True
+
+coprime_checker(32,32)
     
-A = np.array([6, 7, 2, 5])
-
-B = np.array([12, 10, 9 ,4])
-
+A = np.array([3, 7, 32])
+B = np.array([6, 14, 15])
 
 def main(A, B):
-    global counter
-
-
-    #print (len(A),len(B))
     
     tempi = []
     tempj = []
@@ -83,13 +90,13 @@ def deleter(A, B, a,b):
 
     try:
         counter = counter + 1
+        
         A = np.delete(A, a, axis  = 0)
         B = np.delete(B, b, axis  = 0)
         
-        #print ("...DELETED")
-        main(A, B)
+        main(A, B) #now iterate the new arrays
         
     except IndexError: #signal it is over
         print ("The total Counter for this method = %d." % counter)
         
-main(A, B)
+main(A, B) #initiliase the code
